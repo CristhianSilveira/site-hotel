f5ae134b3b44791b999a5d0ebe8e09118dec02a8
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-10-30 20:00:01 -0300

    Editando o menu mobile

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('span').textContent = 'menu';
+        document.querySelector('i').className = 'fa-solid fa-bars';
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('span').textContent = 'close';
+        document.querySelector('i').className = 'fa-solid fa-xmark fa-lg';
     }
 }
