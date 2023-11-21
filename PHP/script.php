56e0d6aefc707cf090080f8bf54087757f4d49ab
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-11-21 11:27:07 -0300

    Adicionando lógica ao PHP

--- PHP/script.php
+++ PHP/script.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reserva Concluída</title>
-    <link rel="stylesheet" href="../CSS/php.css">
+    <link rel="stylesheet" href="../CSS/reservado.css">
 </head>
 
 <body>
